81911576b4195664d776bc322647efcdc5e8e542
deoclaw <115387064+deoclaw@users.noreply.github.com>
2024-09-19 21:36:32 -0500

    added readme and package

---
+++ Assignments/taskmanager/server.js
@@ -0,0 +1,9 @@
+//initialize node project npm init
+// create tasks json file with sampletasks
+// use fs module for file handiling
+
+//getAllTasks - reads json async - return array and handle error
+//listTasks - retrieves tasks with git all task -- format clearly with title and status
+//addTask - title and description param, default not completed status - async update
+//completeTask - title as param - search and update
+//CLI interface for users
---
+++ README.md
@@ -0,0 +1,13 @@
+This repo contains assignments for INF 656
+
+## Directory Structure
+
+```
+Assignment
+    |__taskmanager - the taskmanager assignment
+        |__package-lock.json
+        |__package.json
+        |__server.json
+        |__tasks.json
+README.md
+```
